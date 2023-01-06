e441dab59b85080763b98d8cb72702f17befe18c
Danilo <danilo.volles@outlook.com>
2023-01-05 22:36:15 -0300

    New activity added. Expense chart implementation.

--- app/src/main/java/com/danilovolles/expensetracker/MainActivity.kt
+++ app/src/main/java/com/danilovolles/expensetracker/MainActivity.kt
@@ -4,17 +4,25 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var goPieChart:Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        goPieChart = findViewById(R.id.go_pie_chart)
+
+        goPieChart.setOnClickListener {
+            startActivity(Intent (this, PieChartActivity::class.java))
+        }
     }
 
     fun openAddExpense(view: View) {
-        val intent = Intent (this, AddExpenseScreen::class.java)
-        startActivity(intent)
+        startActivity(Intent (this, AddExpenseScreen::class.java))
     }
 
 }
